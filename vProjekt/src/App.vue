a758803e73a0064d754aa0e9eed0ebc0aa738f26
s-117-22 <s-117-22@gumiforest.ae.krakow.pl>
2022-05-04 17:36:31 +0200

    initial

---
+++ vProjekt/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Lista towarów</h1>
+  <table border="2">
+    <tr>
+      <th>ID towaru</th>
+      <th>Produkt</th>
+      <th>Rozmiar</th>
+      <th>Cena</th>
+      <th>Dodaj do koszyka</th>
+      <th>Usuń z koszyka</th>
+    </tr>
+    <tr v-for="t, index in towary" :key="t.id">
+      <td>{{ index + 1 }}.</td>
+      <td>{{ t.name }}</td>
+      <td>Rozmiar: {{ t.size }}</td>
+      <td>Cena: {{ t.price }}</td>
+      <td><p><button @click="t.stan++">Dodaj</button></p></td>
+      <td><p><button @click="t.stan--">Usuń</button></p></td>
+    </tr>
+  </table>
+  <h1>Koszyk</h1>
+  <table border="5">
+    <tr>
+      <th>ID towaru</th>
+      <th>Produkt</th>
+      <th>Rozmiar</th>
+      <th>Cena</th>
+      <th>Stan</th>
+    </tr>
+    <tr v-for="t, index in towary" :key="t.id">
+      <td>{{ index + 1 }}.</td>
+      <td>{{ t.name }}</td>
+      <td>Rozmiar: {{ t.size }}</td>
+      <td>Cena: {{ t.price }}</td>
+      <td>{{ t.stan }}</td>
+    </tr>
+  </table>
+</template>
+
+
+
+<script setup>
+    import towary from './towary.js'
+</script>
+
---
+++ vProjekt/src/towary.js
@@ -0,0 +1,11 @@
+import { ref } from 'vue'
+export default ref([
+    { id: 1, name: 'Koszula', size: 'S', price: 100, stan:0},
+    { id: 5, name: 'Koszulka', size: 'XL', price: 150, stan:0},
+    { id: 13, name: 'Obuwie', size: 'M', price: 500, stan:0},
+    { id: 16, name: 'Kurtka', size: 'M', price: 252, stan:0},
+    { id: 24, name: 'Plaszcz', size: 'S', price: 300, stan:0},
+    { id: 44, name: 'Spodnie', size: 'L', price: 150, stan:0},
+    { id: 54, name: 'Spodenki', size: 'XXL', price: 75, stan:0},
+    { id: 112, name: 'Czapka', size: 'S', price: 80, stan:0},
+])
